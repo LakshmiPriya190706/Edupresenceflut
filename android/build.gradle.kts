4ed05d9fdd6d2a86531bee9fed1c5a8b736df7f2
LakshmiPriya190706 <lakshmi.priya190706@gmail.com>
2025-05-05 20:46:29 +0530

    apkgit

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,3 @@
-plugins {
-    kotlin("android") version "1.9.23" apply false
-}
-
 buildscript {
     repositories {
         google()
@@ -11,7 +7,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.1")
         classpath("com.google.gms:google-services:4.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22") // ✅ MATCHED
     }
 }
 
@@ -28,9 +24,6 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-
-subprojects {
     project.evaluationDependsOn(":app")
 }
 
